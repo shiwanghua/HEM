75173da1dae8c1852aa24d771089a50f4cc9d076
shiwanghua <2804324275@qq.com>
2021-08-28 02:59:41 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 ulimit -s unlimited
-chmod 777 linuxRB cmakeRB
-./cmakeRB
+#chmod 777 linuxRB cmakeRB
+#./cmakeRB
+expID=$1
+
+for((bits=0;bits<=7;bits++));  #À¨ºÅ²»ÄÜÊ¡
+do  
+expID=$expID+1
+#./cmakeRB $expID $bits
+echo Experiment $expID \done. #$(expr $i \* 3 + 1); 
+done
