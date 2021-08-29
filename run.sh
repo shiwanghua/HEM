094f6859035b93ae593de505620abb72fb91a032
shiwanghua <2804324275@qq.com>
2021-08-29 16:45:59 +0800

    exp

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 ulimit -s unlimited
 sudo chmod 777 cmakeRB
+sudo chmod 777 *.txt
 #./cmakeRB
 expID=$1
 
-for((bits=0;bits<=8;bits++));  #括号不能省
+for((bits=10;bits>=0;bits--));  #括号不能省
 do  
-sudo ./cmakeRB $expID $bits
+./cmakeRB $expID $bits
 echo Experiment $expID \done. #$(expr $i \* 3 + 1); 
 expID=`expr $expID + 1`
 done
