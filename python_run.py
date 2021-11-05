9320ec33be810624c8da81198633265255cb219f
shiwanghua <2804324275@qq.com>
2021-11-05 13:34:53 +0800

    exp1 matching time

--- python_run.py
+++ python_run.py
@@ -4,23 +4,19 @@ os.system('ulimit -s unlimited;sudo chmod 777 cmakeRB')
 os.system('sudo chmod 777 *.txt')
 expID=int(sys.argv[1])
 
-# for m in range(20,9,-2):
-#    os.system('taskset -c 20 ./cmakeRB '+str(expID)+m+' 4 0.3')
-#    print('Experiment',str(expID),' ','atts=20, m= ',str(m),'done.\n\n')
-#    expID+=1
 
 # Exp 1 2
-# for be in range(9,-1,-1):
-#     os.system('taskset -c 18 ./cmakeRB '+str(expID)+' 20 '+str(be)+' 0.3')
-#     print('Experiment',str(expID),' atts=m=',str(20),' be= ',str(be),' w=0.3 done.\n\n')
-#     expID+=1
+for be in range(9,-1,-1):
+    os.system('taskset -c 18 ./cmakeRB '+str(expID)+' 20 '+str(be)+' 0.3')
+    print('Experiment',str(expID),' atts=m=',str(20),' be= ',str(be),' w=0.3 done.\n\n')
+    expID+=1
 
 
 # # Ss size of subscriptions 5-30
-for Ss in range(30,0,-5):
-    os.system('taskset -c 18 ./cmakeRB '+str(expID)+' '+str(Ss)+' 5 0.7')
-    print('Experiment',str(expID),' Ss=',str(Ss),' be= 5, w=0.7 done.\n\n')
-    expID+=1
+# for Ss in range(30,0,-5):
+#     os.system('taskset -c 18 ./cmakeRB '+str(expID)+' '+str(Ss)+' 5 0.7')
+#     print('Experiment',str(expID),' Ss=',str(Ss),' be= 5, w=0.7 done.\n\n')
+#     expID+=1
 
 # # Se size of events 30-80
 # for Se in range(80,20,-10):
