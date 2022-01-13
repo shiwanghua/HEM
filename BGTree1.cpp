01201355e51f38f7d70376e4542ddf174aa3f065
shiwanghua <2804324275@qq.com>
2022-01-13 17:50:40 +0800

    BGTree insert

--- BGTree1.cpp
+++ BGTree1.cpp
@@ -7,8 +7,8 @@
 BGTree1::BGTree1() {
 	numSub = 0;
 	numNode = 0;
-	height=0;
-	maxNodeSize=MAXNodeSIZE;
+	height = 0;
+	maxNodeSize = MAXNodeSIZE;
 	memset(subPredicate, 0, sizeof(subPredicate));
 	roots.resize(atts);
 	_for(i, 0, atts) {
@@ -19,11 +19,13 @@ BGTree1::BGTree1() {
 
 	cout << "ExpID = " << expID << ". BGTree1: maxNodeSize = " << maxNodeSize << "\n";
 }
+
 BGTree1::~BGTree1() {
 	_for(i, 0, atts) {
 		releaseBlueNode(roots[i]);
 	}
 }
+
 void BGTree1::releaseBlueNode(bluenode *&r) {
 	if (r->lowGreenChild) releaseGreenNode(r->lowGreenChild);
 	if (r->highGreenChild) releaseGreenNode(r->highGreenChild);
@@ -31,11 +33,13 @@ void BGTree1::releaseBlueNode(bluenode *&r) {
 	if (r->rightBlueChild) releaseBlueNode(r->rightBlueChild);
 	delete r;
 }
+
 void BGTree1::releaseGreenNode(lgreennode *&r) {
 	if (r->leftChild) releaseGreenNode(r->leftChild);
 	if (r->rightChild) releaseGreenNode(r->rightChild);
 	delete r;
 }
+
 void BGTree1::releaseGreenNode(hgreennode *&r) {
 	if (r->leftChild) releaseGreenNode(r->leftChild);
 	if (r->rightChild) releaseGreenNode(r->rightChild);
@@ -55,39 +59,50 @@ void BGTree1::insert(IntervalSub sub) {
 			nB[i][sub.id] = 1;
 	numSub++;
 }
-void BGTree1::insertIntoBlueNode(bluenode *&r, const int &subID, const int &l, const int &h,const int& attrId, const vector<IntervalSub> &subList) {
+
+void BGTree1::insertIntoBlueNode(bluenode *&r, const int &subID, const int &l, const int &h, const int &attrId,
+								 const vector<IntervalSub> &subList) {
 	r->subids.push_back(subID);
 	r->bst[subID] = 1;
-	if(r->mid==h) r->lMidv.push_back(subID);
-	else if(r->mid+1==l) r->rMidv.push_back(subID);
+	if (r->mid == h) r->lMidv.push_back(subID);
+	else if (r->mid + 1 == l) r->rMidv.push_back(subID);
 
-	if(r->subids.size()>maxNodeSize){ ;
-		r->leftBlueChild=new bluenode(l,h,numNode++,r->levelid+1, nullptr, nullptr, nullptr, nullptr);
-		r->rightBlueChild=new bluenode(l,h,numNode++,r->levelid+1, nullptr, nullptr, nullptr, nullptr);
-		r->lowGreenChild=new lgreennode(l,h,numNode++,r->levelid+1,nullptr,nullptr);
-		r->highGreenChild=new hgreennode(l,h,numNode++,r->levelid+1,nullptr,nullptr);
-		for(auto&& id:r->subids){
-			for(auto&& c:subList[id].constraints){
-				if(c.att==attrId){
-					if(c.highValue<=r->mid){
+	if(r->leftBlueChild!= nullptr){ // has child
+		if (h <= r->mid) {
+				insertIntoBlueNode(r->leftBlueChild, subID, l, h, attrId, subList);
+		} else if (l > r->mid) {
+				insertIntoBlueNode(r->rightBlueChild, subID, l, h, attrId, subList);
+		} else {
+				insertIntoGreenNode(r->lowGreenChild, subID, l, attrId, subList);
+				insertIntoGreenNode(r->highGreenChild, subID, h, attrId, subList);
+			}
+	}else if(r->subids.size() > maxNodeSize){ // no child and too big node
+		r->leftBlueChild = new bluenode(r->l, r->mid, ++numNode, r->levelid + 1, nullptr, nullptr, nullptr, nullptr);
+		r->rightBlueChild = new bluenode(r->mid+1, r->h, ++numNode, r->levelid + 1, nullptr, nullptr, nullptr, nullptr);
+		r->lowGreenChild = new lgreennode(r->l, r->mid, ++numNode, r->levelid + 1, nullptr, nullptr);
+		r->highGreenChild = new hgreennode(r->mid+1, r->h, ++numNode, r->levelid + 1, nullptr, nullptr);
+		for (auto &&id: r->subids) {
+			for (auto &&c: subList[id].constraints) {
+				if (c.att == attrId) {
+					if (c.highValue <= r->mid) {
 						r->leftBlueChild->subids.push_back(id);
-						r->leftBlueChild->bst[id]=1;
-						if(r->leftBlueChild->mid==h) r->leftBlueChild->lMidv.push_back(subID);
-						else if(r->leftBlueChild->mid+1==l) r->leftBlueChild->rMidv.push_back(subID);
-					}else if(c.lowValue>r->mid){
+						r->leftBlueChild->bst[id] = 1;
+						if (r->leftBlueChild->mid == c.highValue) r->leftBlueChild->lMidv.push_back(subID);
+						else if (r->leftBlueChild->mid + 1 == c.lowValue) r->leftBlueChild->rMidv.push_back(subID);
+					} else if (c.lowValue > r->mid) {
 						r->rightBlueChild->numNodeSub++;
 						r->rightBlueChild->subids.push_back(id);
-						r->rightBlueChild->bst[id]=1;
-						if(r->rightBlueChild->mid==h) r->rightBlueChild->lMidv.push_back(subID);
-						else if(r->rightBlueChild->mid+1==l) r->rightBlueChild->rMidv.push_back(subID);
-					}else{ // l<=mid<mid+1<=h
+						r->rightBlueChild->bst[id] = 1;
+						if (r->rightBlueChild->mid == c.highValue) r->rightBlueChild->lMidv.push_back(subID);
+						else if (r->rightBlueChild->mid + 1 == c.lowValue) r->rightBlueChild->rMidv.push_back(subID);
+					} else { // l<=mid<mid+1<=h
 						r->lowGreenChild->subids.push_back(id);
-						r->lowGreenChild->bst[id]=1;
-						if(r->lowGreenChild->mid+1==l)
+						r->lowGreenChild->bst[id] = 1;
+						if (r->lowGreenChild->mid + 1 == c.lowValue)
 							r->lowGreenChild->midv.push_back(id);
 						r->highGreenChild->subids.push_back(id);
-						r->highGreenChild->bst[id]=1;
-						if(r->highGreenChild->mid==h)
+						r->highGreenChild->bst[id] = 1;
+						if (r->highGreenChild->mid == c.highValue)
 							r->highGreenChild->midv.push_back(id);
 					}
 					break;
@@ -95,43 +110,76 @@ void BGTree1::insertIntoBlueNode(bluenode *&r, const int &subID, const int &l, c
 			}
 		}
 	}
-	if(h<=r->mid){
-		if(r->leftBlueChild!= nullptr)
-			insertIntoBlueNode(r->leftBlueChild, subID, l, h,attrId,subList);
-	}
-	else if(l>r->mid){
-		if(r->rightBlueChild!= nullptr)
-			insertIntoBlueNode(r->rightBlueChild,subID,l,h,attrId,subList);
-	}
-	else{
-		if(r->lowGreenChild!= nullptr){
-			insertIntoGreenNode(r->lowGreenChild,subID,l,attrId,subList);
-			insertIntoGreenNode(r->highGreenChild,subID,h,attrId,subList);
+}
+void BGTree1::insertIntoGreenNode(lgreennode *&r, const int &subID, const int &l, const int &attrId,
+								  const vector<IntervalSub> &subList) {
+	r->subids.push_back(subID);
+	r->bst[subID] = 1;
+	if(r->mid+1==l)
+		r->midv.push_back(subID);
+
+	if (r->leftChild != nullptr) {  // 有左子必有右子节点
+		if (l <= r->mid)
+			insertIntoGreenNode(r->leftChild, subID, l, attrId, subList);
+		else insertIntoGreenNode(r->rightChild, subID, l, attrId, subList);
+	}else if(r->subids.size()>maxNodeSize){
+		r->leftChild=new lgreennode(r->l,r->mid,++numNode,r->levelid+1, nullptr, nullptr);
+		r->rightChild=new lgreennode(r->mid+1,r->h,++numNode,r->levelid+1, nullptr, nullptr);
+		for (auto &&id: r->subids) {
+			for (auto &&c: subList[id].constraints) {
+				if (c.att == attrId) {
+					if(c.lowValue<=r->mid){
+						r->leftChild->subids.push_back(id);
+						r->leftChild->bst[id]=1;
+						if(r->leftChild->mid+1==c.lowValue)
+							r->leftChild->midv.push_back(id);
+					}else{
+						r->rightChild->subids.push_back(id);
+						r->rightChild->bst[id]=1;
+						if(r->rightChild->mid+1==c.lowValue)
+							r->rightChild->midv.push_back(id);
+					}
+					break;
+				}
+			}
 		}
 	}
 }
-void BGTree1::insertIntoGreenNode(lgreennode *&r, const int &subID, const int &l,const int& attrId, const vector<IntervalSub> &subList) {
+void BGTree1::insertIntoGreenNode(hgreennode *&r, const int &subID, const int &h, const int &attrId,
+								  const vector<IntervalSub> &subList) {
 	r->subids.push_back(subID);
 	r->bst[subID] = 1;
+	if(r->mid==h)
+		r->midv.push_back(subID);
+
 	if (r->leftChild != nullptr) {  // 有左子必有右子节点
-		if (l <= r->mid)
-			insertIntoGreenNode(r->leftChild, subID, l);
-		else insertIntoGreenNode(r->rightChild, subID, l);
+		if (h < r->mid)
+			insertIntoGreenNode(r->leftChild, subID, h, attrId, subList);
+		else insertIntoGreenNode(r->rightChild, subID, h, attrId, subList);
+	}else if(r->subids.size()>maxNodeSize){
+		r->leftChild=new hgreennode(r->l,r->mid-1,++numNode,r->levelid+1, nullptr, nullptr);
+		r->rightChild=new hgreennode(r->mid,r->h,++numNode,r->levelid+1, nullptr, nullptr);
+		for (auto &&id: r->subids) {
+			for (auto &&c: subList[id].constraints) {
+				if (c.att == attrId) {
+					if(c.highValue<r->mid){
+						r->leftChild->subids.push_back(id);
+						r->leftChild->bst[id]=1;
+						if(r->leftChild->mid==c.highValue)
+							r->leftChild->midv.push_back(id);
+					}else{
+						r->rightChild->subids.push_back(id);
+						r->rightChild->bst[id]=1;
+						if(r->rightChild->mid==c.highValue)
+							r->rightChild->midv.push_back(id);
+					}
+					break;
+				}
+			}
+		}
 	}
 }
-void BGTree1::insertIntoGreenNode(hgreennode *&r, const int &subID, const int &h,const int& attrId, const vector<IntervalSub> &subList) {
-	if (r->bst == nullptr) {
-		r->subids.push_back(subID);
-		if (r->numNodeSub >= boundaryNumSub) //r->subids.size()==r->numNodeSub
-			vectorToBitset(r->subids, r->bst);
-	} else {
-		(*(r->bst))[subID] = 1;
-	}
 
-	if (r->leftChild != nullptr) {
-		if (h >= r->mid)
-			insertIntoGreenNode(r->rightChild, subID, h);
-		else insertIntoGreenNode(r->leftChild, subID, h);
-	}
+bool BGTree1::deleteSubscription(IntervalSub sub){
+
 }
-
