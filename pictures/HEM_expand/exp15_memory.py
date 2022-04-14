59a3eee01b96db6de7bc5b2198967125eca17971
shiwanghua <2804324275@qq.com>
2022-04-15 00:43:37 +0800

    pictures

--- pictures/HEM_expand/exp15_memory.py
+++ pictures/HEM_expand/exp15_memory.py
@@ -9,14 +9,16 @@ rc('mathtext', default='regular')
 plt.rcParams['axes.unicode_minus'] = False
 plt.rcParams['font.family'] = ['Times New Roman']  #
 Name = ["REIN", "HEM", "Simple", "TAMA", "Ada-REIN", "OpIndex"]
-x = ["0.3m", "1m", "3m", "5m", "7m", "9m"]
+x = ["0.3M", "1M", "3M", "5M", "7M", "9M"]
 
-Rein = [45, 152, 457, 762, 1068, 1373]
-HEM = [90, 302, 908, 1513, 2119, 2725]
-Simple = [36, 122, 366, 610, 854, 1098]
-TAMA = [141, 470, 1410, 2350, 3291, 4231]
+REIN = [46, 153, 458, 763, 1069, 1374]
+HEM5DD = [69, 227, 680, 1133, 1586, 2039]
+HEM5_VAS = [69, 228, 681, 1134, 1587, 2040]
+TAMA = [146, 487, 1461, 2435, 3409, 4384]
 AdaREIN = [45, 152, 457, 762, 1068, 1373]
+Simple = [36, 122, 366, 610, 854, 1098]
 OpIndex = [34, 114, 343, 572, 801, 1029]
+
 lsize = 24
 
 fig = plt.figure(figsize=(5, 4))
@@ -24,8 +26,8 @@ ax = fig.add_subplot(111)
 ax.set_xlabel('Number of Subscriptions', fontsize=lsize)
 ax.set_ylabel('Memory Consumption (MB)', fontsize=lsize)
 # plt.xticks(range(0,10))
-ax.plot(x, Rein, marker='v', color='r', label=Name[0])
-ax.plot(x, HEM, marker='.', color='DODGERBLUE', label=Name[1])
+ax.plot(x, REIN, marker='v', color='r', label=Name[0])
+ax.plot(x, HEM5_VAS, marker='.', color='DODGERBLUE', label=Name[1])
 # ax.plot(x, Simple, marker='D', color='deepskyblue', label=Name[2]) #
 ax.plot(x, TAMA, marker='*', color='DarkCyan', label=Name[3])
 ax.plot(x, AdaREIN, marker='x', color='DarkMagenta', label=Name[4])
